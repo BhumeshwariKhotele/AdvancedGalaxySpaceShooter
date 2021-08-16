05c9620e8fd385ad487b590726fb7b0bf2f5d99d
BhumeshwariKhotele <bhumeshwari.khotele@xcubelabs.com>
2021-08-16 17:12:42 +0530

    Player Movement done

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float PlayerMoveSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
+
+    }
+
+    private void PlayerMove()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * Time.deltaTime * PlayerMoveSpeed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * PlayerMoveSpeed * verticalInput);
+
+
+        //player Bounds for y direction
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        //player Bounds for y direction
+
+
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
     }
 }
