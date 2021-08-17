f8fb608868e5e8fd9a5f148995eaec45b06f6710
BhumeshwariKhotele <bhumeshwari.khotele@xcubelabs.com>
2021-08-17 10:39:44 +0530

    created method for player movement

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float PlayerMoveSpeed;
-    private float horizontalInput;
-    private float verticalInput;
-    public GameObject laserPrefab;
-    public float fireRate = 0.25f;
-    public float canFire = 0;
-
-
+   
+    [SerializeField] float moveSpeed;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float canfire = 0f;
+    [SerializeField] float fireRate = 0.25f;
+    Vector3 directionKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,46 +19,58 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerMove();
+        //Player input movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Key(Vector3.right, horizontal);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Key(Vector3.up, vertical);
+        }
+        //Player  Boundarys
+        XYDirection(transform.position.x, transform.position.y);
+        //Instantiating laser
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
         {
-            if (Time.time > canFire)
+            if (Time.time > canfire)
             {
-                Instantiate(laserPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
-                canFire = Time.time + fireRate;
+                Instantiate(laserPrefab, transform.position + new Vector3(0, 1f, 0), Quaternion.identity);
+                canfire = Time.time + fireRate;
             }
+
         }
 
     }
 
-    private void PlayerMove()
+    private void XYDirection(float Xval, float Yval)
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * Time.deltaTime * PlayerMoveSpeed * horizontalInput);
-        transform.Translate(Vector3.up * Time.deltaTime * PlayerMoveSpeed * verticalInput);
-
-
-        //player Bounds for y direction
-        if (transform.position.y > 0)
+        if (Yval > 0)
         {
-            transform.position = new Vector3(transform.position.x, 0, 0);
+            transform.position = new Vector3(Xval, 0, 0);
         }
-        else if (transform.position.y <= -4.2f)
+        else if (Yval < -4.1f)
         {
-            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+            transform.position = new Vector3(Xval, -4.1f, 0);
         }
-
-        //player Bounds for y direction
-
-
-        if (transform.position.x > 9.5f)
+        if (Xval >= 10f)
         {
-            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+            transform.position = new Vector3(-10f, Yval, 0);
         }
-        else if (transform.position.x < -9.5f)
+        else if (Xval <= -10f)
         {
-            transform.position = new Vector3(9.5f, transform.position.y, 0);
+            transform.position = new Vector3(10f, Yval, 0);
         }
+    }
+    public void Key(Vector3 vector, float axis)
+    {
+
+        transform.Translate(vector * Time.deltaTime * moveSpeed * axis);
     }
 }
+
+
+
